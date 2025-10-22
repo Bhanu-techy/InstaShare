7d2fadd38ab9b87667773e3de97acccc003e5342
Bhanu-techy <bhanuprakashdevari@gmail.com>
2025-10-22 20:40:31 +0530

    added like interactivity

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,19 +1,40 @@
+import { useState } from 'react'
 import {Link} from 'react-router-dom'
+import {BsHeart} from 'react-icons/bs'
+import {FaRegComment} from 'react-icons/fa'
+import {BiShareAlt} from 'react-icons/bi'
+import {FcLike} from 'react-icons/fc'
+
 import './index.css'
 
 const PostItem = props => {
 
     const {details} = props
 
+    const [likeBtn, setLikeBtn] = useState(false)
+
     const {
       postDetails = {},
       userId,
+      postId,
       profilePic,
       likesCount,
       userName,
       comments = [],
       createdAt,
     } = details
+
+    const [count, setCount] = useState(likesCount)
+
+    const onClickLike = (id) => {
+      setLikeBtn((prevState) => !prevState)
+
+      if(likeBtn){
+        setCount(likesCount)
+      }else{
+        setCount(likesCount+1)
+      }
+    }
 
   return (
     <li>
@@ -29,7 +50,42 @@ const PostItem = props => {
         </div>
         <img src={postDetails.image_url} className="post-img" alt="post" />
         <div className="post-content">
-          <p>{likesCount} likes</p>
+          <div>
+            {likeBtn ? (
+              <button
+                type="button"
+                data-testid="unLikeIcon"
+                className="like-button"
+                onClick={() => onClickLike(postId)}
+              >
+                <FcLike height={50} />
+              </button>
+            ) : (
+              <button
+                type="button"
+                className="like-button"
+                data-testid="likeIcon"
+                onClick={() => onClickLike(postId)}
+              >
+                <BsHeart height={50} />
+              </button>
+            )}
+            <button
+              type="button"
+              data-testid="commentIcon"
+              className="like-button"
+            >
+              <FaRegComment height={50} />
+            </button>
+            <button
+              type="button"
+              data-testid="shareIcon"
+              className="like-button"
+            >
+              <BiShareAlt height={50} />
+            </button>
+          </div>
+          <p>{count} likes</p>
           <p>{postDetails.caption}</p>
           <div>
             {comments.map(each => (
